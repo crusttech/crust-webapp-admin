db9d144eeb7563c23e6f025c3b97e65dc20b3998
Denis Arh <denis.arh@gmail.com>
2019-10-28 19:41:04 +0100

    Rename subscription alert component

--- src/main.js
+++ src/main.js
@@ -7,12 +7,12 @@ import './themes'
 // When you run this in development mode with corteza repository/package linked
 // (yarn link ...) it tries to work with 2 instances of vue and throws:
 //
-// Unknown custom element: <the-subscription-navbar> - did you register the component
+// Unknown custom element: <the-subscription-alert> - did you register the component
 // correctly? For recursive components, make sure to provide the "name" option.
 Vue.component('the-subscription-alert', TheSubscriptionAlert)
 
 app({
   // Extending base Admin app with an additional header to handle
   // subscription notifications
-  template: `<div id="admin" class="crust"><the-subscription-navbar /><router-view/></div>`,
+  template: `<div id="admin" class="crust"><the-subscription-alert /><router-view/></div>`,
 })
