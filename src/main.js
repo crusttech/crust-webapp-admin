a2911caef58065006306f01c7ec27d4b786fcbc7
Denis Arh <denis.arh@gmail.com>
2019-10-28 19:34:55 +0100

    Add subscription alert

--- src/main.js
+++ src/main.js
@@ -1,4 +1,18 @@
+import Vue from 'vue'
+
+import TheSubscriptionAlert from './components/TheSubscriptionAlert'
 import app from 'corteza-webapp-admin/src/app'
 import './themes'
 
-app()
+// When you run this in development mode with corteza repository/package linked
+// (yarn link ...) it tries to work with 2 instances of vue and throws:
+//
+// Unknown custom element: <the-subscription-navbar> - did you register the component
+// correctly? For recursive components, make sure to provide the "name" option.
+Vue.component('the-subscription-alert', TheSubscriptionAlert)
+
+app({
+  // Extending base Admin app with an additional header to handle
+  // subscription notifications
+  template: `<div id="admin" class="crust"><the-subscription-navbar /><router-view/></div>`,
+})
